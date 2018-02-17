9fac6c9bccec2dcf9bb6037ed15b9e352b65a2c9
andresgsepulveda <andres.sepulveda808@gmail.com>
2018-02-16 22:38:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -157,7 +157,7 @@ bot.on("message", function(message) {
 		    .then(collected =>
 			  console.log(collected.size))
 		    .catch(collected => 
-			   message.channel.sendMessage('Accountability is ${collected.size} of ${collected.size}, present or accounted for.));
+			   message.channel.sendMessage("Accountability is ${collected.size} of ${collected.size}, present or accounted for."));
     		    break;
 	}
 });
