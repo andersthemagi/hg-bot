6cf9f9f84f723c08586b74865c98c6e92db01f12
andresgsepulveda <andres.sepulveda808@gmail.com>
2018-02-16 17:22:04 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ bot.on("message", function(message) {
             //IF THE NEXT ARGUMENT INCLUDES VULGAR
             if (args[1] === "vulgar") {
 
-                var choice = Math.floor(Math.random() * 3 + 1);
+                var choice = Math.floor(Math.random() * 6 + 1);
                 // CHOOSING WHICH VULGARITY TO USE
                 if (choice === 1) {
                     message.channel.sendMessage("Holy fuck open the fucking door? @everyone");
@@ -33,8 +33,17 @@ bot.on("message", function(message) {
                 else if (choice === 2) {
                     message.channel.sendMessage("Can someone open the fucking door? @everyone");
                 }
-                else if (choice === 2) {
+                else if (choice === 3) {
                     message.channel.sendMessage("HEY FUCKHEAD. DOOR. NOW. @everyone");
+                }
+                else if (choice === 4) {
+                    message.channel.sendMessage("COME HERE SHITBAG. SOMEONE NEEDS A DOOR OPENING @everyone");
+                }
+                else if (choice === 5) {
+                    message.channel.sendMessage("I CHIMED IN WITH A 'HAVEN'T YOU PEOPLE EVER HEARD OF, OPEN THE GODDAMN DOOR, NO?' @everyone");
+                }
+                else if (choice === 6) {
+                    message.channel.sendMessage("Fuck. Shit. Ass. Shit. Damn. Door. @everyone");
                 }
                 break;
             }
