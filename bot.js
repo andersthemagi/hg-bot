13d5a858d16472be447e3f508ac428689f01b778
andresgabrielsepulveda <andres.sepulveda808@gmail.com>
2018-04-18 12:12:55 -0700

    testing feedback command

--- bot.js
+++ bot.js
@@ -167,49 +167,44 @@ bot.on("message", function(message) {
     if((message.channel.type === "dm") && message.content.includes("feedback"))
     {
         var feedbackGate = false;
+        var nameGetGate = false;
         message.author.send("Would you like to give feedback? (Yes/No)");
         const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {max: 1, time: 30000});
         console.log(collector);
-        collector.on('collect', newMessage => {
-            if (newMessage.content.includes('yes') || newMessage.content.includes('Yes'))
+        collector.on('collect', fbMessage => {
+            if (fbMessage.content.toLowerCase().includes('yes'))
             {
-                message.author.send("Would you like to be anonymous? (Yes / No)");
-                const anonCollect = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {max: 1, time: 30000});
-                var nameString = "ANONYMOUS";
-                console.log(anonCollect);
-                anonCollect.on('collect', anonMessage => {
-                    if (anonMessage.content.includes('yes') || anonMessage.content.includes('Yes'))
-                    {
-                        message.author.send("Your name will be kept in confidentiality.");
-                    }
-                    else if (anonMessage.content.includes('no') || anonMessage.content.includes('No'))
-                    {
-                        message.author.send("What's your name? Please don't put anything stupid. ");
-                        const nameCollect = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {max: 1, time: 30000});
-                        console.log(nameCollect);
-                        nameCollect.on('collect', nameMessage => {
-                            nameString = nameMessage.toString();
-                        });
-                    }
-                    nameString += "\n\n";
-                    var responseString = "None.";
-                    message.channel.send("What would you like to give as feedback? You have 5 minutes to write whatever you need. (I highly recommend writing what you need to say before hand and pasting it into the chat with me) :");
-                    const responseCollect = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id, {max: 1, time: 1000 * 60 * 5});
-                    console.log(responseCollect);
-                    responseCollect.on('collect', responseMessage => {
-                        responseString = responseMessage.toString();
-                        message.channel.send("Wonderful! Thanks for your feedback! Your message will be sent to the Protocol Officer for review. Have a wonderful day! :smile: :heart: ");
-                        nameString += responseString;
-                    });
-                });
-
-
+                message.channel.send("Wonderful!");
+                feedbackGate = true;
             }
-            else if (newMessage.content.includes('no') || newMessage.content.includes('No'))
+            else if (fbMessage.content.toLowerCase().includes('no'))
             {
-                message.author.send("Then have a great rest of your day! Here's a cat picture to make you feel wonderful! :smile: :heart:");
+                message.channel.send("Then have a wonderful rest of your day!");
             }
         });
+        if (feedbackGate)
+        {
+            message.channel.send("Would you like to stay anonymous?");
+            collector.next.then(anonMessage => {
+                if (anonMessage.content.toLowerCase().includes('yes'))
+                {
+                    message.channel.send("Then your secret is safe with me!");
+                }
+                else if (anonMessage.content.toLowerCase().includes('no'))
+                {
+                    message.channel.send("What is your name?: ");
+                    nameGetGate = true;
+                }
+            }).catch(err => {
+                message.channel.send("Sorry! Time ran out!");
+            });
+            feedbackGate = false;
+        }
+        else if (nameGetGate)
+        {
+            message.channel.send("Wow this is fun!");
+        }
+
     }
 
     if (message.author.equals(bot.user)) {return;}
